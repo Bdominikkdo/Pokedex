f5b0b0c733f92abdbb8a8cc276b3dec0e369dd57
dominikkdo <bdelleon8@xarxa.fedac.cat>
2026-04-24 18:52:06 +0200

    cambios

--- pokedex/src/main.jsx
+++ pokedex/src/main.jsx
@@ -1,5 +1,32 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.jsx'
+router.jsx
 
-render(<App />, document.getElementById('app'))
+
+import { createBrowserRouter } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+main.jsx
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
